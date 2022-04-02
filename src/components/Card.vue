1bc7dbbb24c262c6ae60b4368860683a1853b3a1
Woosang <yws1502@naver.com>
2022-04-03 01:45:54 +0900

    ✨ Card component 생성

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Card
+    <div>card id : {{cid}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    cid() {
+      return this.$route.params.cid;
+    }
+  }
+};
+</script>
+
+<style></style>
