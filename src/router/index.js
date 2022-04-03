29d742ab56b02d7395fe707a4032af0ebdd77a10
Woosang <yws1502@naver.com>
2022-04-03 23:20:24 +0900

    ✨ 네비게이션 가드 기능 구현

--- src/router/index.js
+++ src/router/index.js
@@ -8,15 +8,22 @@ import NotFound from "../components/NotFound.vue";
 
 Vue.use(VueRouter);
 
+const requireAuth = (to, from, next) => {
+  const isAuth = localStorage.getItem("token");
+  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`;
+  isAuth ? next() : next(loginPath);
+};
+
 const router = new VueRouter({
   mode: "history",
   routes: [
-    { path: "/", component: Home },
+    { path: "/", component: Home, beforeEnter: requireAuth },
     { path: "/login", component: Login },
     {
       path: "/b/:bid",
       component: Board,
-      children: [{ path: "c/:cid", component: Card }]
+      beforeEnter: requireAuth,
+      children: [{ path: "c/:cid", component: Card, beforeEnter: requireAuth }]
     },
     { path: "*", component: NotFound }
   ]
