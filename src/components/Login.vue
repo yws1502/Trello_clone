4f572d03e665260adb831015ccecc5cb1e0a1d68
Woosang <yws1502@naver.com>
2022-04-03 23:33:16 +0900

    💄 로그인 페이지 마크업 및 스타일 적용

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    Login
+  <div class="login">
+    <h2>Log in to Trello</h2>
+    <form>
+      <div>
+        <label for="email">Email</label>
+        <input
+          type="text"
+          class="form-control"
+          name="email"
+          autofocus
+          placeholder="e.g., test@test.com"
+        />
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input type="password" class="form-control" placeholder="123123" />
+      </div>
+      <button class="btn" type="submit">
+        Log In
+      </button>
+    </form>
+    <p class="error"></p>
   </div>
 </template>
 
@@ -8,4 +28,12 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+.login {
+  width: 400px;
+  margin: 0 auto;
+}
+.error {
+  color: #f00;
+}
+</style>
